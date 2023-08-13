f74f6b33c36a2b4c397f6d30fa7d187d6b363b99
HeartLezz7 <heartchantapath@gmail.com>
2023-08-13 17:06:28 +0700

    finish-JS05-01-printPrime

--- 01-Function-Declaration/05-printPrime/main.js
+++ 01-Function-Declaration/05-printPrime/main.js
@@ -1,21 +1,21 @@
 let num = prompt("Enter number");
 
-function isPrime(num) {
+function isPrime(n) {
     let isPrime = true;
-    for (let divider = 2; divider < num; divider++) {
-        if(num % divider == 0) {
-            return false;
+    for (let divider = 2; divider < n; divider++) {
+        if(n % divider == 0) {
+            isPrime = false;
+            break;
         }
     }
-    return true;
+    return isPrime;
 }
+
 
 function printPrime(num) {
+    let pp = '';
     for (let n = 2; n <= num; n++){
-        isPrime(n);
+        if(isPrime(n)) {pp += n}
     }
-    
+    return pp;
 }
-
-// alert(isPrime(+num))
-// console.log(isPrime(+num))
